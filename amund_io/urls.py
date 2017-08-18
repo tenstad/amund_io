20893d31462d6e06824bbf525439b37255bbbfda
Amund Tenstad <github@amund.io>
2017-08-18 11:21:55 +0200

    Create index page

--- amund_io/urls.py
+++ amund_io/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from .views import Index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^markdownx/', include('markdownx.urls')),
+    url(r'', Index.as_view()),
 ]
