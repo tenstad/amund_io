aa2e2510a4459eb9f835dd6a1d402ff6b35aa070
Amund Tenstad <github@amund.io>
2017-08-18 11:58:37 +0200

    Add ingress, tags and thumbnail to articles

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from markdownx.models import MarkdownxField
+from files.models import Image
+from tags.models import Tag
 
 class Article(models.Model):
     title = models.CharField(max_length=50)
+    ingress = MarkdownxField()
     content = MarkdownxField()
+    thumbnail = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_thumb')
+    tags = models.ManyToManyField(Tag, blank=True)
     creation_date = models.DateTimeField(default=timezone.now)
