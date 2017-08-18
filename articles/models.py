91689938ef1a11f8081aa7df6cfef16b7518cf59
Amund Tenstad <github@amund.io>
2017-08-18 12:03:45 +0200

    Make ingress and content optional

--- articles/models.py
+++ articles/models.py
@@ -6,8 +6,8 @@ from tags.models import Tag
 
 class Article(models.Model):
     title = models.CharField(max_length=50)
-    ingress = MarkdownxField()
-    content = MarkdownxField()
+    ingress = MarkdownxField(blank=True)
+    content = MarkdownxField(blank=True)
     thumbnail = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True, related_name='%(app_label)s_%(class)s_related_thumb')
     tags = models.ManyToManyField(Tag, blank=True)
     creation_date = models.DateTimeField(default=timezone.now)
